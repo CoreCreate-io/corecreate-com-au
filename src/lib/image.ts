12a45386a7de5ce782120065cebde81ee5bf9970
CoreCreate.io <josh@corecreate.io>
2025-05-11 21:42:31 +1000

    typescript fixes for image.ts

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,14 +1,25 @@
 import imageUrlBuilder from '@sanity/image-url';
 import { client } from './sanity.client';
 
-// Define a type for Sanity image references
+// Define a more specific type for Sanity image sources
 interface SanityImageSource {
+  _type: string;
   asset: {
     _ref: string;
     _type: string;
   };
-  _type: string;
-  [key: string]: any; // For any additional properties
+  hotspot?: {
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+  };
+  crop?: {
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  };
 }
 
 const builder = imageUrlBuilder(client);
