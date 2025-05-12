1bf92d56d93e61a0a74145dce219f2b61413ebd5
CoreCreate.io <josh@corecreate.io>
2025-05-12 11:00:31 +1000

    correct import globals css

--- src/components/ProjectsGrids.tsx
+++ src/components/ProjectsGrids.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import '../styles/globals.css';
+import './globals.css';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
